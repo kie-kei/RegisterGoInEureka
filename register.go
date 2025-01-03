cc5ebb31d45a0bb8248cf1f0f4ec31c12c442962
Maksim Pozdeev <pozdeev_max@mail.ru>
2025-01-03 21:38:05 +0400

    add required data to request

--- register.go
+++ register.go
@@ -7,37 +7,63 @@ import (
 	"net/http"
 )
 
-// Конфигурация для регистрации в Eureka
 type EurekaConfig struct {
-	EurekaURL     string
-	InstanceId    string
-	AppName       string
-	HostName      string
-	IPAddr        string
-	Port          int
-	SecurePort    int
-	StatusPageUrl string
+	EurekaURL        string
+	InstanceId       string
+	AppName          string
+	HostName         string
+	IPAddr           string
+	Port             int
+	SecurePort       int // Теперь это необязательный параметр
+	StatusPageUrl    string
+	HomePageUrl      string
+	HealthCheckUrl   string
+	VipAddress       string
+	SecureVipAddress string
+	DataCenterName   string // Новый параметр
 }
 
-// Структура для регистрации экземпляра в Eureka
 type EurekaInstance struct {
 	Instance struct {
-		InstanceId string `json:"instanceId"`
-		HostName   string `json:"hostName"`
-		App        string `json:"app"`
-		IpAddr     string `json:"ipAddr"`
-		Port       struct {
+		InstanceId       string `json:"instanceId"`
+		HostName         string `json:"hostName"`
+		App              string `json:"app"`
+		IpAddr           string `json:"ipAddr"`
+		VipAddress       string `json:"vipAddress"`
+		SecureVipAddress string `json:"secureVipAddress"`
+		StatusPageUrl    string `json:"statusPageUrl"`
+		HomePageUrl      string `json:"homePageUrl"`
+		HealthCheckUrl   string `json:"healthCheckUrl"`
+		Port             struct {
 			Value int `json:"$"`
 		} `json:"port"`
 		SecurePort struct {
 			Value int `json:"$"`
-		} `json:"securePort"`
-		StatusPageUrl string `json:"statusPageUrl"`
+		} `json:"securePort,omitempty"` // Убираем обязательность для SecurePort
+		DataCenterInfo struct {
+			Class string `json:"@class"`
+			Name  string `json:"name"`
+		} `json:"dataCenterInfo"`
 	} `json:"instance"`
 }
 
-// Функция для регистрации экземпляра в Eureka
 func RegisterInstance(config EurekaConfig) error {
+	if config.HomePageUrl == "" {
+		config.HomePageUrl = config.StatusPageUrl
+	}
+	if config.HealthCheckUrl == "" {
+		config.HealthCheckUrl = config.StatusPageUrl
+	}
+	if config.VipAddress == "" {
+		config.VipAddress = config.AppName
+	}
+	if config.SecureVipAddress == "" {
+		config.SecureVipAddress = config.AppName
+	}
+	if config.DataCenterName == "" {
+		config.DataCenterName = "MyOwn"
+	}
+
 	// Формируем структуру запроса
 	instance := EurekaInstance{}
 	instance.Instance.InstanceId = config.InstanceId
@@ -45,10 +71,18 @@ func RegisterInstance(config EurekaConfig) error {
 	instance.Instance.HostName = config.HostName
 	instance.Instance.IpAddr = config.IPAddr
 	instance.Instance.StatusPageUrl = config.StatusPageUrl
+	instance.Instance.HomePageUrl = config.HomePageUrl
+	instance.Instance.HealthCheckUrl = config.HealthCheckUrl
+	instance.Instance.VipAddress = config.VipAddress
+	instance.Instance.SecureVipAddress = config.SecureVipAddress
 	instance.Instance.Port.Value = config.Port
-	instance.Instance.SecurePort.Value = config.SecurePort
 
-	// Преобразуем структуру в JSON
+	if config.SecurePort != 0 {
+		instance.Instance.SecurePort.Value = config.SecurePort
+	}
+	instance.Instance.DataCenterInfo.Class = "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo"
+	instance.Instance.DataCenterInfo.Name = config.DataCenterName
+
 	data, err := json.Marshal(instance)
 	if err != nil {
 		return fmt.Errorf("Ошибка при маршалинге данных: %v", err)
